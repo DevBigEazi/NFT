470967e0b2afb5ff70c5d1d20b0f8ad8f9f8afd3
devbigeazi <adesholatajudeen1@gmail.com>
2024-10-25 16:20:17 +0100

    deployed

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.ts
 ```
+
+```
+Contract Addresses
+
+NFTModule#NFTMarket - 0x531aB60351ac0E8ca9C91De822E28E09458d6e33
+```
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract NFTMarket {
+    address public owner;
+    address public buyer;
 
-contract NFTMarket is ERC721, Ownable {
     enum OrderStatus {
         None,
         Created,
@@ -21,56 +21,36 @@ contract NFTMarket is ERC721, Ownable {
     Asset[] public assets;
     mapping(uint256 => bool) public isSold;
 
-    event AssetListed(
-        string indexed name,
-        uint16 price,
-        uint256 indexed tokenId
-    );
-    event AssetSold(
-        string indexed name,
-        uint16 price,
-        address indexed buyer,
-        uint256 indexed tokenId
-    );
+    event AssetListed(string indexed name, uint16 price);
+    event AssetSold(string indexed name, uint16 price, address buyer);
 
-    constructor() ERC721("DMark", "DMK") {}
-
-    function createItem(string memory _name, uint16 _price) external onlyOwner {
-        require(msg.sender != address(0), "Zero address is not allowed");
-        require(_price > 0, "Price must be greater than zero");
-
-        Asset memory newAsset = Asset({
-            name: _name,
-            price: _price,
-            status: OrderStatus.Created
-        });
-
-        assets.push(newAsset);
-        uint256 tokenId = assets.length - 1;
-
-        _mint(owner(), tokenId);
-
-        emit AssetListed(_name, _price, tokenId);
+    constructor() {
+        owner = msg.sender;
     }
 
-    function purchaseItem(uint8 _index) external payable {
+    function listOrder(string memory _name, uint16 _price) external {
+        require(msg.sender != address(0), "Zero address is not allowed");
+
+        Asset memory newAsset;
+        newAsset.name = _name;
+        newAsset.price = _price;
+        newAsset.status = OrderStatus.Created;
+
+        assets.push(newAsset);
+
+        emit AssetListed(_name, _price);
+    }
+
+    function buyOrder(uint8 _index) external {
+        require(msg.sender != address(0), "Zero address is not allowed");
         require(_index < assets.length, "Out of bound!");
-        Asset storage asset = assets[_index];
+        require(!isSold[_index], "Asset already sold");
 
-        require(
-            asset.status == OrderStatus.Created,
-            "Asset is not available for sale"
-        );
-        require(msg.value == asset.price, "Incorrect amount sent");
-
-        asset.status = OrderStatus.Sold;
+        
+        assets[_index].status = OrderStatus.Sold;
         isSold[_index] = true;
+        buyer = msg.sender;
 
-        _transfer(owner(), msg.sender, _index);
-
-        (bool sent, ) = owner().call{value: msg.value}("");
-        require(sent, "Failed to send Ether");
-
-        emit AssetSold(asset.name, asset.price, msg.sender, _index);
+        emit AssetSold(assets[_index].name, assets[_index].price, msg.sender);
     }
 }
